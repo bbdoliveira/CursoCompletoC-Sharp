8f921508e5ca7f7fb160a769c34869859c198a7a
Bruno Bonifácio de Oliveira <bbdoliveira@gmail.com>
2021-08-23 12:05:06 -0300

    Nivelamento Exercicio 01

---
+++ Nivelamento/Nivelamento/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace Nivelamento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int codPeca, quantidadePeca, count = 0;
+            double valorPeca, somaPreco = 0.0;
+
+
+
+            while (count < 2)
+            {
+                string[] vet = Console.ReadLine().Split(' ');
+                codPeca = int.Parse(vet[0]);
+                quantidadePeca = int.Parse(vet[1]);
+                valorPeca = double.Parse(vet[2]);
+                somaPreco += valorPeca * quantidadePeca;
+                count++;
+            }
+
+            Console.WriteLine("VALOR A PAGAR: R$ " + somaPreco.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
