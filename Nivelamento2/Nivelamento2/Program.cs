3403a51eee53d98bb8348e5f1720dfbcf09b2ff5
Bruno Bonifácio de Oliveira <bbdoliveira@gmail.com>
2021-08-23 12:06:49 -0300

    Nivelamento Exercicio 02

---
+++ Nivelamento2/Nivelamento2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace Nivelamento2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double raio, valorPi, areaCirculo;
+
+            valorPi = 3.14159;
+
+            raio = double.Parse(Console.ReadLine());
+
+            areaCirculo = valorPi * Math.Pow(raio, 2.0);
+
+            Console.WriteLine("A=" + areaCirculo.ToString("F4", CultureInfo.InvariantCulture));
+        }
+    }
+}
