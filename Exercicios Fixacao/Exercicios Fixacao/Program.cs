925d8ed08da99fba8a160bc33b58571d96af1986
Bruno Bonifácio de Oliveira <bbdoliveira@gmail.com>
2021-08-23 12:44:41 -0300

    Criando a pasta de exercicios de Fixação

---
+++ Exercicios Fixacao/Exercicios Fixacao/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exercicios_Fixacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Raiz dos Exercícios de Fixação");
+        }
+    }
+}
