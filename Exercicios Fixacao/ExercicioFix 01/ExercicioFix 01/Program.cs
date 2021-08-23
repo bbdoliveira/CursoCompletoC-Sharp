b2a8e971c8b0506345a432bdca75bdbf5dbc6300
Bruno Bonifácio de Oliveira <bbdoliveira@gmail.com>
2021-08-23 13:53:10 -0300

    Exercicio Fix 01 Resolvido

---
+++ Exercicios Fixacao/ExercicioFix 01/ExercicioFix 01/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercicioFix_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo r;
+            r = new Retangulo();
+
+            Console.WriteLine("Entre com a largura e altura do retângulo: ");
+            r.Largura = double.Parse(Console.ReadLine());
+            r.Altura = double.Parse(Console.ReadLine());
+
+            double area = r.Area();
+            double perimetro = r.Perimetro();
+            double diagonal = r.Diagonal();
+
+            Console.WriteLine("AREA: " + area.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("PERIMETRO: " + perimetro.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("DIAGONAL: " + diagonal.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
---
+++ Exercicios Fixacao/ExercicioFix 01/ExercicioFix 01/Retangulo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExercicioFix_01
+{
+    class Retangulo
+    {
+        public double Largura, Altura;
+
+        public double Area()
+        {
+            return Largura * Altura;
+        }
+
+        public double Perimetro()
+        {
+            return 2 * (Largura + Altura);
+        }
+
+        public double Diagonal()
+        {
+            return Math.Sqrt(Math.Pow(Largura, 2) + Math.Pow(Altura, 2));
+        }
+    }
+}
