9c6e839e1f09b856bd63c72527f963f60df49158
Bruno Bonifácio de Oliveira <bbdoliveira@gmail.com>
2021-09-07 14:01:43 -0300

    Atualizando Exercicio usando membros estaticos.

--- Exercicios Resolvidos/Exercicio 05/Exercicio 05/Exercicio 05/Program.cs
+++ Exercicios Resolvidos/Exercicio 05/Exercicio 05/Exercicio 05/Program.cs
@@ -5,28 +5,17 @@ namespace Exercicio_05
 {
     class Program
     {
-        static double Pi = 3.14;
         static void Main(string[] args)
         {
             Console.Write("Entre o valor do raio: ");
             double raio = double.Parse(Console.ReadLine());
 
-            double circ = Circunferencia(raio);
-            double volume = Volume(raio);
+            double circ = Calculadora.Circunferencia(raio);
+            double volume = Calculadora.Volume(raio);
 
             Console.WriteLine("Circunferência: " + circ.ToString("F2", CultureInfo.InvariantCulture));
             Console.WriteLine("Volume: " + volume.ToString("F2", CultureInfo.InvariantCulture));
-            Console.WriteLine("Valor de PI: " + Pi.ToString("F2", CultureInfo.InvariantCulture));
-        }
-
-        static double Circunferencia(double r)
-        {
-            return 2.0 * Pi * r;
-        }
-
-        static double Volume(double r)
-        {
-            return 4.0 / 3.0 * Pi * Math.Pow(r, 3);
-        }
+            Console.WriteLine("Valor de PI: " + Calculadora.Pi.ToString("F2", CultureInfo.InvariantCulture));
+        }        
     }
 }
