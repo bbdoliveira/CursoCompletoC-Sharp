245b3373dde1e4f14601d431174ee338d7092a65
Bruno Bonifácio de Oliveira <bbdoliveira@gmail.com>
2021-09-07 14:38:03 -0300

    Exercicio Fix 04 Resolvido

---
+++ Exercicios Fixacao/ExercicioFix 04/ExercicioFix 04/ExercicioFix 04/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercicioFix_04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Qual é a cotação do dólar? ");
+            double dolar = double.Parse(Console.ReadLine());
+            Console.Write("Quantos dólares você vai comprar? ");
+            double qteDolares = double.Parse(Console.ReadLine());
+
+            double valorPago = ConversorDeMoeda.Moeda(qteDolares, dolar);
+
+            Console.WriteLine("Calor a ser pago em reais = " + valorPago.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
