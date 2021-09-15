44290e90a5ba0ae75cfa7c69a3ab6739bd3124c9
Bruno Bonifácio de Oliveira <bbdoliveira@gmail.com>
2021-09-15 01:20:45 -0300

    Estruturando Exercicio Conta Bancaria

---
+++ Exercicios Fixacao/ExercicioFix 06/ExercicioFix Conta Bancaria/ExercicioFix Conta Bancaria/ContaBancaria.cs
@@ -0,0 +1,12 @@
+﻿using System.Globalization;
+
+namespace ExercicioFix_Conta_Bancaria
+{
+    class ContaBancaria
+    {
+        public int Numero { get; private set; }
+        private string Titular;
+        private double Saldo;
+
+    }
+}
--- Exercicios Fixacao/ExercicioFix 06/ExercicioFix Conta Bancaria/ExercicioFix Conta Bancaria/Program.cs
+++ Exercicios Fixacao/ExercicioFix 06/ExercicioFix Conta Bancaria/ExercicioFix Conta Bancaria/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace ExercicioFix_Conta_Bancaria
 {
@@ -6,7 +7,25 @@ namespace ExercicioFix_Conta_Bancaria
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Entre o número da conta: ");
+            int numero = int.Parse(Console.ReadLine());
+            Console.Write("Entre o titular da conta: ");
+            string nome = Console.ReadLine();
+            Console.Write("Haverá depósito inicial (s/n): ");
+            char deposito = char.Parse(Console.ReadLine());
+
+            if (deposito == 's')
+            {
+                Console.Write("Entre o de depósito inicial: ");
+                double depInicial = double.Parse(Console.ReadLine());
+                Console.Write("Seu saldo inicial é: " + depInicial.ToString("F2", CultureInfo.InvariantCulture));
+            }
+            else
+            {
+                double saldoInicial = 0;
+                Console.Write("Seu saldo incial é: " + saldoInicial.ToString("F2", CultureInfo.InvariantCulture));
+            }
+
         }
     }
 }
