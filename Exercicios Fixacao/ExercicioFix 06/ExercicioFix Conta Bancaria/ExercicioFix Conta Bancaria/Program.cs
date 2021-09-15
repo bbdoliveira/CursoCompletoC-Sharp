27ffc8a16e8fdebd0692509cc3c2eae6df905fb6
Bruno Bonifácio de Oliveira <bbdoliveira@gmail.com>
2021-09-15 00:48:57 -0300

    Criando Projeto Conta Bancaria

---
+++ Exercicios Fixacao/ExercicioFix 06/ExercicioFix Conta Bancaria/ExercicioFix Conta Bancaria/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExercicioFix_Conta_Bancaria
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
