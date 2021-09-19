845802e34906b8f2d9bf8c7fea0f5c589be536ca
Bruno Bonifácio de Oliveira <bbdoliveira@gmail.com>
2021-09-19 13:31:38 -0300

    Exercicio 01 Vetores

---
+++ Exercicios Resolvidos/Exercicio 07 Vetores Pt1/Couse/Couse/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+namespace Couse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            double[] vect = new double[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                vect[i] = double.Parse(Console.ReadLine());
+            }
+
+            double soma = 0.0;
+            for (int i = 0; i < n; i++)
+            {
+                soma += vect[i];
+            }
+
+            double avg = soma / n;
+
+            Console.WriteLine("AVERAGE HEIGHT = " + avg.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
