0728b95ed20433d72d33a8717221cf9ad68a5369
Bruno Bonifácio de Oliveira <bbdoliveira@gmail.com>
2021-09-19 14:02:23 -0300

    Exercicio Resolvido de Vetores com objeto do tipo referencia

---
+++ Exercicios Resolvidos/Exercicio 08 Vetores de Classes/Couse/Couse/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+
+namespace Couse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            double sum = 0.0;
+
+            Product[] vect = new Product[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                //Váriaveis temporárias
+                string name = Console.ReadLine();
+                double price = double.Parse(Console.ReadLine());
+
+                vect[i] = new Product(name, price); //Utilizando o construtor da classe.
+                sum += vect[i].Price;
+            }
+
+            double avg = sum / n;
+
+            Console.WriteLine("AVERAGE PRICE = " + avg.ToString("F2", CultureInfo.InvariantCulture));
+
+            /*for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Produto: " + vect[i].Name + ", Valor: " + vect[i].Price);
+            }*/
+        }
+    }
+}
