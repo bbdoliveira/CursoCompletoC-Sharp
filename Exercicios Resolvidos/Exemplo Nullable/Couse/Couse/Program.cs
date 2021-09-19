fb0c759c658fe0352db5487cb2d0df3372917c7c
Bruno Bonifácio de Oliveira <bbdoliveira@gmail.com>
2021-09-19 12:15:38 -0300

    Exemplo de Nullable como usar

---
+++ Exercicios Resolvidos/Exemplo Nullable/Couse/Couse/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Couse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double? x = null; //Defime uma variavel como Nullable de double usando o '?'.
+            double? y = 10.0;
+
+            //Metodo GetValueOrDeafault(), pega o valor que tiver ou então o valor padrão do tipo.
+            Console.WriteLine(x.GetValueOrDefault());
+            Console.WriteLine(y.GetValueOrDefault());
+
+            //Método HasValue() verifica se tem ou não Valor retornando um booleano (True or False)
+            Console.WriteLine(x.HasValue);
+            Console.WriteLine(y.HasValue);
+
+            //Método Value() retorna o valor da váriavel, porém se o valor for null é preciso tratar o erro
+            //pois ela não vai imprimir null.
+            if (x.HasValue)
+            {
+                Console.WriteLine(x.Value);
+            }
+            else
+            {
+                Console.WriteLine("X is null");
+            }
+
+            if (y.HasValue)
+            {
+                Console.WriteLine(y.Value);
+            }
+            else
+            {
+                Console.WriteLine("Y is null");
+            }
+        }
+    }
+}
---
+++ Exercicios Resolvidos/Exemplo Struct/Couse/Couse/Point.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Couse
+{
+    struct Point
+    {
+        public double X;
+        public double Y;
+
+        public Point(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public override string ToString()
+        {
+            return "(" + X + ", " + Y + ")";
+        }
+    }
+}
