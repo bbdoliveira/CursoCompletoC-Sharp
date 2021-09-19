9e6590996627d6a75d4647bb44c969fb56345e9c
Bruno Bonifácio de Oliveira <bbdoliveira@gmail.com>
2021-09-19 12:21:20 -0300

    Exemplo do operador de coalescência nula

---
+++ Exercicios Resolvidos/Exemplo Operador Coalescencia nula/Couse/Couse/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Couse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double? x = null;
+            double? y = 10;
+
+            //Usando este operador, caso o valor de X seja null ele vai usar o valor a direita.
+            double a = x ?? 5; 
+            double b = y ?? 5;
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+        }
+    }
+}
