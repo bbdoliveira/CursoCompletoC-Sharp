0d462f215244d2abed4dfbf30fc945e699c64085
Bruno Bonifácio de Oliveira <bbdoliveira@gmail.com>
2021-09-19 17:30:43 -0300

    Exercicio fix Array Corrigido

---
+++ Exercicios Fixacao/ExercicioFix 07/Couse/Couse/Guest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Couse
+{
+    class Guest
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        public Guest(string name, string email)
+        {
+            Name = name;
+            Email = email;
+        }
+
+        public override string ToString()
+        {
+            return Name + ", " + Email;
+        }
+    }
+}
---
+++ Exercicios Fixacao/ExercicioFix 07/Couse/Couse/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Couse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Quantos quartos serão alugados? ");
+            int n = int.Parse(Console.ReadLine());
+
+            int[] estudantes = new int[10];
+            Guest[] vect = new Guest[10];
+
+            for (int i = 0; i < n; i++)
+            {
+                int entradas = i + 1;
+                Console.WriteLine("Aluguel #" + entradas + ":");
+                Console.Write("Nome: ");
+                string name = Console.ReadLine();
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+                Console.Write("Quarto: ");
+                int room = int.Parse(Console.ReadLine());
+
+                vect[room] = new Guest(name, email);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Quartos ocupados:");
+            for (int i = 0; i < 10; i++)
+            {
+                if (vect[i] != null)
+                {
+                    Console.WriteLine(i + ": " + vect[i]);
+                }
+            }
+        }
+    }
+}
