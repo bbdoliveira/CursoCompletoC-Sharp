6878d291b705599f7ef2648987d7b9e5a7df0d3d
Bruno Bonifácio de Oliveira <bbdoliveira@gmail.com>
2021-10-10 23:47:12 -0300

    Exercicio Fixação Listas Resolvido

---
+++ Exercicios Fixacao/ExercicioFix 08/Course/Course/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int qte;
+            Console.Write("How many employees will be registrered? ");
+            qte = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            List<Employee> list = new List<Employee>();
+
+            for (int i = 1; i <= qte; i++)
+            {
+                Console.WriteLine("Employee #" + i + ":");
+                Console.Write("Id: ");
+                int id = int.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Salary: ");
+                double salary = double.Parse(Console.ReadLine());
+                list.Add(new Employee(id, name, salary));
+                Console.WriteLine();
+            }
+
+            Console.Write("Enter the employee id that will have salary increase: ");
+            int searchId = int.Parse(Console.ReadLine());
+
+            Employee emp = list.Find(x => x.Id == searchId);
+            if (emp != null)
+            {
+                Console.Write("Enter the percentage: ");
+                double percentage = double.Parse(Console.ReadLine());
+                emp.IncreaseSalary(percentage);
+            }
+            else
+            {
+                Console.WriteLine("This id does not exist!");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Updated list of employees: ");
+            foreach (Employee item in list)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
