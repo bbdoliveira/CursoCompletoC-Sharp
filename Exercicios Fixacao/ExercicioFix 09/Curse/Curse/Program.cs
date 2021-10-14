32c2842e7eea8a5d5dba53eb43f08e7d48474339
Bruno Bonifácio de Oliveira <bbdoliveira@gmail.com>
2021-10-13 23:54:10 -0300

    Exercicio Matrizes Resolvido

---
+++ Exercicios Fixacao/ExercicioFix 09/Curse/Curse/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Curse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int m, n;
+
+
+        }
+    }
+}
---
+++ Exercicios Resolvidos/Exercicio 11 Matrizes/Course/Course/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] mat = new int[n, n];
+
+            for (int i = 0; i < n; i++)
+            {
+                //Fazer a leitura dos dados do vetor com espaço
+                string[] values = Console.ReadLine().Split(' ');
+
+                for (int j = 0; j < n; j++)
+                {
+                    mat[i, j] = int.Parse(values[j]);
+                }
+            }
+
+            //Imprimir a diágonal principal
+            Console.WriteLine("Main diagonal: ");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(mat[i, i] + " ");
+            }
+            Console.WriteLine();
+
+            //Verificando quantos números negativos tem na matriz
+            int count = 0;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (mat[i, j] < 0)
+                    {
+                        count++;
+                    }
+                }
+            }
+            Console.WriteLine("Negative numbers: " + count);
+        }
+    }
+}
