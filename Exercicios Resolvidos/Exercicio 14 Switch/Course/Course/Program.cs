de19d28ec49ebc524890c8e950636ead25a5af2e
Bruno Bonifácio de Oliveira <bbdoliveira@gmail.com>
2021-10-16 20:25:53 -0300

    Exercicio Resolvido Switch

---
+++ Exercicios Resolvidos/Exercicio 14 Switch/Course/Course/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um numero de 1 a 7: ");
+            int x = int.Parse(Console.ReadLine());
+
+            string day;
+
+            switch (x)
+            {
+                case 1:
+                    day = "Sunday";
+                    break;
+                case 2:
+                    day = "Monday";
+                    break;
+                case 3:
+                    day = "Tuesday";
+                    break;
+                case 4:
+                    day = "Wednesday";
+                    break;
+                case 5:
+                    day = "Thursday";
+                    break;
+                case 6:
+                    day = "Friday";
+                    break;
+                case 7:
+                    day = "Saturday";
+                    break;
+                default:
+                    day = "Invalid value!!!";
+                    break;
+            }
+
+            Console.WriteLine("Day: " + day);
+        }
+    }
+}
