375b8217426cb7e03f9b8689104b5b8edb678931
Bruno Bonifácio de Oliveira <bbdoliveira@gmail.com>
2021-10-16 20:14:39 -0300

    Exercicio sobre declaracao de variavel com var

---
+++ Exercicios Resolvidos/Exercicio 13 Variavel (VAR)/Course/Course/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declarando váriaveis sem definir o tipo usando o tipo VAR.
+            var x = 10;
+            var y = 20.0;
+            var z = "Maria";
+
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(z);
+        }
+    }
+}
