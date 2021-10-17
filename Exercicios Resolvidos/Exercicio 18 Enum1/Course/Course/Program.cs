c593a2e00cc6702e1fd20287c8d4dd632d2a48cf
Bruno Bonifácio de Oliveira <bbdoliveira@gmail.com>
2021-10-17 16:17:33 -0300

    Criadas a classe Order e o Enum OrderStatus

---
+++ Exercicios Resolvidos/Exercicio 18 Enum1/Course/Course/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Course.Entities;
+using Course.Entities.Enums;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order
+            {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
+        }
+    }
+}
