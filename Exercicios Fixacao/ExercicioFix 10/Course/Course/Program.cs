8bcf31327c8ef0db805bbd26799144652e8d55c3
Bruno Bonifácio de Oliveira <bbdoliveira@gmail.com>
2021-12-13 00:44:56 -0300

    Exercicio 21 Exemplo de Heranca

--- Exercicios Fixacao/ExercicioFix 10/Course/Course/Program.cs
+++ Exercicios Fixacao/ExercicioFix 10/Course/Course/Program.cs
@@ -20,15 +20,30 @@ namespace Course
             Console.Write("Status: ");
             OrderStatus orderStatus = Enum.Parse<OrderStatus>(Console.ReadLine());
             
-            Console.Write("How mani items to this order? ");
-            int quantity = int.Parse(Console.ReadLine());
-            Console.Write("Product name: ");
+            Console.Write("How many items to this order? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Enter #{i} item data: ");
+                Console.Write("Product name: ");
+                string productName = Console.ReadLine();
+                Console.Write("Product price: ");
+                double productPrice = double.Parse(Console.ReadLine());
+                Console.Write("Quantity: ");
+                int quantity = int.Parse(Console.ReadLine());
+                Product product = new Product(productName, productPrice);
+                OrderItem item = new OrderItem(quantity, productPrice);
+                double totalOrder = item.SubTotal(quantity, productPrice);
+            }
+
+            Client client = new Client(userName, userEmail, userBirthDay);
 
             Console.WriteLine(userName);
             Console.WriteLine(userEmail);
             Console.WriteLine(userBirthDay);
             Console.WriteLine(orderStatus);
-            Console.WriteLine(quantity);
+            Console.WriteLine(n);
         }
     }
 }
