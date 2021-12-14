c48c8873aa9a4a63bbd981303e70be60e9b20f41
Bruno Bonifácio de Oliveira <bbdoliveira@gmail.com>
2021-12-14 13:30:29 -0300

    Criacao da classe Product

---
+++ Exercicios Fixacao/ExercicioFix 11/Course/Course/Entities/Product.cs
@@ -0,0 +1,24 @@
+﻿using System.Globalization;
+namespace Course.Entities
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product()
+        {
+        }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public virtual string PriceTag()
+        {
+            return Price.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Exercicios Fixacao/ExercicioFix 11/Course/Course/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Globalization;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            double teste = 200.00;
+            string imprima = teste.ToString("F2", CultureInfo.InvariantCulture);
+            Console.WriteLine(imprima);
+        }
+    }
+}
