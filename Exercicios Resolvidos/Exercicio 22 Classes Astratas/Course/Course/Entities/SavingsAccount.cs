550d9136e8c259e31eaa559e4023b5c92880b59c
Bruno Bonifácio de Oliveira <bbdoliveira@gmail.com>
2021-12-14 22:23:46 -0300

    Iniciando estudo Classes Abstratas

---
+++ Exercicios Resolvidos/Exercicio 22 Classes Astratas/Course/Course/Entities/SavingsAccount.cs
@@ -0,0 +1,27 @@
+﻿namespace Course.Entities
+{
+    class SavingsAccount : Account
+    {
+        public double InterestRate { get; set; }
+
+        public SavingsAccount()
+        {
+        }
+
+        public SavingsAccount(int number, string holder, double balance, double interestRate) : base(number, holder, balance)
+        {
+            InterestRate = interestRate;
+        }
+
+        public void UpdateBalance()
+        {
+            Balance += Balance * InterestRate; 
+        }
+
+        public override void Witchdraw(double amount) //Método de saque diferente para SavingsAccount
+        {
+            base.Witchdraw(amount);
+            Balance -= 2.0;
+        }
+    }
+}
---
+++ Exercicios Resolvidos/Exercicio 22 Classes Astratas/Course/Course/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Course.Entities;
+using System.Globalization;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account acc1 = new Account(1001, "Maria", 500.0);
+            Account acc2 = new SavingsAccount(1002, "Jose", 500.0, 0.01);
+
+            acc1.Witchdraw(10.0);
+            acc2.Witchdraw(10.0);
+
+            Console.WriteLine(acc1.Balance);
+            Console.WriteLine(acc2.Balance);
+        }
+    }
+}
