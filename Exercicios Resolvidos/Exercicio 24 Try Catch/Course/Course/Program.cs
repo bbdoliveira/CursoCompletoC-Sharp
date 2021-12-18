27bf46ec3f9cc14cffcbe09533009a38a1adbeb4
Bruno Bonifácio de Oliveira <bbdoliveira@gmail.com>
2021-12-18 02:43:24 -0300

    Exemplo de bloco Try-Catch

---
+++ Exercicios Resolvidos/Exercicio 24 Try Catch/Course/Course/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Type a number: ");
+                int n1 = int.Parse(Console.ReadLine());
+                Console.Write("Type a number: ");
+                int n2 = int.Parse(Console.ReadLine());
+
+                int div = n1 / n2;
+
+                Console.Write("Result: " + div);
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Division by zero is now allowed!");
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("Format Error! " + e.Message);
+            }
+        }
+    }
+}
