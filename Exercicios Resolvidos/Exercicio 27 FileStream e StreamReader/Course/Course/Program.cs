af58878ad2b9ff64be8996426f698be3c7ab5331
Bruno Bonifácio de Oliveira <bbdoliveira@gmail.com>
2021-12-20 13:08:23 -0300

    Terminado Exercicio sobre FileStream e StreamReader

---
+++ Exercicios Resolvidos/Exercicio 27 FileStream e StreamReader/Course/Course/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"c:\temp\file.txt";
+            StreamReader sr = null;
+
+            try
+            {
+                sr = File.OpenText(path);
+                while (!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine();
+                    Console.WriteLine(line);
+                } 
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An erro accurred");
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                if (sr != null) sr.Close();
+            }
+        }
+    }
+}
