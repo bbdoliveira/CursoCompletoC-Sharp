45f489cf3d2c921181ad3ae8485777f64a479719
Bruno Bonifácio de Oliveira <bbdoliveira@gmail.com>
2021-12-23 12:43:51 -0300

    Finalizando exercicio sobre bloco Using

---
+++ Exercicios Resolvidos/Exercicio 28 Bloco Using/Course/Course/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"c:\temp\file.txt";
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string line = sr.ReadLine();
+                        Console.WriteLine(line);
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error accurred!");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
