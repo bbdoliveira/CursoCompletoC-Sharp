99e6431a8fed13cf6ac3fc5ab41846f71fd7d015
Bruno Bonifácio de Oliveira <bbdoliveira@gmail.com>
2022-01-09 22:42:56 -0300

    Termino do Exercicio Fix de Arquivos

---
+++ Exercicios Fixacao/ExercicioFix 13/Course/Course/Entities/Product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course.Entities
+{
+    internal class Product
+    {
+        public string Name { get; set; }
+        public double UnityPrice { get; set; }
+        public int Quantity { get; set; }
+
+        public Product(string name, double unityPrice, int quantity)
+        {
+            Name = name;
+            UnityPrice = unityPrice;
+            Quantity = quantity;
+        }
+
+        public double Total()
+        {
+            return UnityPrice * Quantity;
+        }
+    }
+}
--- Exercicios Fixacao/ExercicioFix 13/Course/Course/Program.cs
+++ Exercicios Fixacao/ExercicioFix 13/Course/Course/Program.cs
@@ -1,2 +1,35 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+Console.Write("Enter file full path (Ex: C:NomeDoArquivo.csv): ");
+string sourceFilePath = Console.ReadLine();
+
+try
+{
+    string[] lines = File.ReadAllLines(sourceFilePath);
+
+    string sourceFolderPath = Path.GetDirectoryName(sourceFilePath);
+    string targetFolderPath = sourceFolderPath + @"\out";
+    string targetFilePath = targetFolderPath + @"\summary.csv";
+
+    Directory.CreateDirectory(targetFolderPath);
+
+    using (StreamWriter sw = File.AppendText(targetFilePath))
+    {
+        foreach (string line in lines)
+        {
+            string[] fields = line.Split(',');
+            string name = fields[0];
+            double price = double.Parse(fields[1], System.Globalization.NumberStyles.Currency);
+            int quantity = int.Parse(fields[2]);
+
+            Course.Entities.Product prod = new Course.Entities.Product(name, price, quantity);
+
+            sw.WriteLine(prod.Name + ", " + prod.Total());
+        }
+    }
+}
+catch (IOException e)
+{
+    Console.WriteLine("An error ocurred!");
+    Console.WriteLine(e.Message);
+}
