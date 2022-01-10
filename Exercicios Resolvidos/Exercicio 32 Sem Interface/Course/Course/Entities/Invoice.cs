fc775160f27cd1d462b9534980c47d56c97ac4c7
Bruno Bonifácio de Oliveira <bbdoliveira@gmail.com>
2022-01-10 01:31:57 -0300

    Exercicio resolvido sem Interface Parte 1

---
+++ Exercicios Resolvidos/Exercicio 32 Sem Interface/Course/Course/Entities/Invoice.cs
@@ -0,0 +1,33 @@
+﻿using System.Globalization;
+
+namespace Course.Entities
+{
+    internal class Invoice
+    {
+        public double BasicPayment { get; set; }
+        public double Tax { get; set; }
+
+        //Construtores
+        public Invoice(double basicPayment, double tax)
+        {
+            BasicPayment = basicPayment;
+            Tax = tax;
+        }
+
+        //Propriedade Calculada
+        public double TotalPayment
+        {
+            get { return BasicPayment + Tax; }
+        }
+
+        public override string ToString()
+        {
+            return "Basic payment: "
+                + BasicPayment.ToString("F2", CultureInfo.InvariantCulture)
+                + "\nTax: "
+                + Tax.ToString("F2", CultureInfo.InvariantCulture)
+                + "\nTotal Payment: "
+                + TotalPayment.ToString(CultureInfo.InvariantCulture);
+        }
+    }
+}
--- Exercicios Resolvidos/Exercicio 32 Sem Interface/Course/Course/Program.cs
+++ Exercicios Resolvidos/Exercicio 32 Sem Interface/Course/Course/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Globalization;
+using Course.Entities;
+
+Console.WriteLine("Enter rental data");
+Console.Write("Car model:");
+string model = Console.ReadLine();
+Console.Write("Pickup (dd/MM/yyyy hh:mm): "); //Na linha abaixo passando o formato da data e hora.
+DateTime start = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+Console.Write("Return (dd/MM/yyyy hh:mm): ");
+DateTime finish = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+
+CarRental carRental = new CarRental(start, finish, new Vehicle(model));
+
+Console.Write("Enter price per hour: ");
+double priceHour = double.Parse(Console.ReadLine());
+Console.Write("Enter price per day: ");
+double priceDay = double.Parse(Console.ReadLine());
