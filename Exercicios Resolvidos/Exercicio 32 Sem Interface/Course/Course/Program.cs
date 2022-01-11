2826ae75febc8a95f94204fe040f1c42da00605e
Bruno Bonifácio de Oliveira <bbdoliveira@gmail.com>
2022-01-11 14:42:40 -0300

    Terminando o Exercicio sem Interfaces.

--- Exercicios Resolvidos/Exercicio 32 Sem Interface/Course/Course/Program.cs
+++ Exercicios Resolvidos/Exercicio 32 Sem Interface/Course/Course/Program.cs
@@ -1,17 +1,25 @@
 ﻿using System.Globalization;
 using Course.Entities;
+using Course.Services;
 
 Console.WriteLine("Enter rental data");
-Console.Write("Car model:");
+Console.Write("Car model: ");
 string model = Console.ReadLine();
 Console.Write("Pickup (dd/MM/yyyy hh:mm): "); //Na linha abaixo passando o formato da data e hora.
 DateTime start = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
 Console.Write("Return (dd/MM/yyyy hh:mm): ");
 DateTime finish = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
 
+Console.Write("Enter price per hour: ");
+double hour = double.Parse(Console.ReadLine());
+Console.Write("Enter price per day: ");
+double day = double.Parse(Console.ReadLine());
+
 CarRental carRental = new CarRental(start, finish, new Vehicle(model));
 
-Console.Write("Enter price per hour: ");
-double priceHour = double.Parse(Console.ReadLine());
-Console.Write("Enter price per day: ");
-double priceDay = double.Parse(Console.ReadLine());
+RentalService rentalService = new RentalService(hour, day);
+
+rentalService.ProcessInvoice(carRental);
+
+Console.WriteLine("INVOICE");
+Console.WriteLine(carRental.Invoice);
