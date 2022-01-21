91570405f757a21964939f21d545acbb8307fcaf
Bruno Bonifácio de Oliveira <bbdoliveira@gmail.com>
2022-01-20 23:59:16 -0300

    Exercicio Generics part 1 Reuso

---
+++ Exercicios Resolvidos/Exercicio 33 Generics/Course/Course/Program.cs
@@ -0,0 +1,17 @@
+﻿using Course.Services;
+
+PrintService printService = new PrintService();
+
+Console.Write("How many values? ");
+int n = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < n; i++)
+{
+    int x = int.Parse(Console.ReadLine());
+    printService.AddValue(x);
+}
+
+printService.Print();
+Console.Write("First: " + printService.First());
+
+
