2b59d6d2bcc5e0ed054750086691c5232c8a96e9
Bruno Bonifácio de Oliveira <bbdoliveira@gmail.com>
2022-01-25 15:02:34 -0300

    Exercicio concluido de Hash para Tipo Referencia e Struct

--- Exercicios Resolvidos/Exercicio 38 Colecoes Hash/Course/Course/Program.cs
+++ Exercicios Resolvidos/Exercicio 38 Colecoes Hash/Course/Course/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Course.Entities;
+
+HashSet<Product> a = new HashSet<Product>();
+a.Add(new Product("TV", 900.00));
+a.Add(new Product("Notebook", 1200.00));
+
+HashSet<Point> b = new HashSet<Point>();
+b.Add(new Point(3, 4));
+b.Add(new Point(5, 10));
+
+Product prod = new Product("Notebook", 1200.00);
+Console.WriteLine(a.Contains(prod));
+
+Point c = new Point(5, 10);
+Console.WriteLine(b.Contains(c));
