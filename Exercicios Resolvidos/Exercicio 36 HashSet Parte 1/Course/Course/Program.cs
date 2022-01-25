e397afba9b5a16c03c5b3fe3383132cc47415a5e
Bruno Bonifácio de Oliveira <bbdoliveira@gmail.com>
2022-01-25 13:25:59 -0300

    Exercicio de HashSet parte 1

---
+++ Exercicios Resolvidos/Exercicio 36 HashSet Parte 1/Course/Course/Program.cs
@@ -0,0 +1,15 @@
+﻿HashSet<string> set = new HashSet<string>();
+
+set.Add("TV");
+set.Add("Notebook");
+set.Add("Tablet");
+
+if (set.Contains("Tablet") == true) //Verifica se existe o valor no conjunto.
+{
+    Console.WriteLine("Mas é claro!");
+}
+
+foreach (string item in set) //Imprime o conjunto linha a linha.
+{
+    Console.WriteLine(item);
+}
