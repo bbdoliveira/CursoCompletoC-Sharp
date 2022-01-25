e090019f0783ae1d12a80347eb5e723cea58d7da
Bruno Bonifácio de Oliveira <bbdoliveira@gmail.com>
2022-01-25 13:09:00 -0300

    Conclusão do exercicio de Equals e GetHashCode

---
+++ Exercicios Resolvidos/Exercicio 35 GetHashCode e Equals/Course/Course/Entities/Client.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course.Entities
+{
+    internal class Client
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        public override bool Equals(object? obj)
+        {
+            if (!(obj is Client)) //Verifica se o objeto é do tipo da classe Client.
+            {
+                return false;
+            }
+            Client other = obj as Client; //Seta o objeto como cliente.
+            return Email.Equals(other.Email);
+        }
+
+        public override int GetHashCode() 
+        {
+            return Email.GetHashCode(); //Qual objeto que eu vou comparar para saber se são iguais.
+        }
+    }
+}
---
+++ Exercicios Resolvidos/Exercicio 35 GetHashCode e Equals/Course/Course/Program.cs
@@ -0,0 +1,9 @@
+﻿using Course.Entities;
+
+Client a = new Client { Name = "Maria", Email = "maria@gmail.com" };
+Client b = new Client { Name = "Alex", Email = "maria@gmail.com" };
+
+Console.WriteLine(a.Equals(b)); //Equals compara o conteudo das variáveis.
+Console.WriteLine(a == b);      //O operador == comprar a referencia do ponteiro de memoria.
+Console.WriteLine(a.GetHashCode());
+Console.WriteLine(b.GetHashCode());
