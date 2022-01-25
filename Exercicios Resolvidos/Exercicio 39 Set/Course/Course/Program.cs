7ac308f31a64780142273c791b5c6af6492ee73d
Bruno Bonifácio de Oliveira <bbdoliveira@gmail.com>
2022-01-25 15:59:55 -0300

    Exercicio concluido de SET utilizando arquivos.

---
+++ Exercicios Resolvidos/Exercicio 39 Set/Course/Course/Program.cs
@@ -0,0 +1,25 @@
+﻿using Course.Entities;
+
+HashSet<LogRecord> records = new HashSet<LogRecord>();
+
+Console.Write("Enter file full path: ");
+string path = Console.ReadLine();
+
+try
+{
+    using (StreamReader sr = File.OpenText(path))
+    {
+        while (!sr.EndOfStream)
+        {
+            string[] line = sr.ReadLine().Split(' ');
+            string name = line[0];
+            DateTime dateTime = DateTime.Parse(line[1]);
+            records.Add(new LogRecord { Username = name, Instant = dateTime});
+        }
+        Console.WriteLine("Total users: " + records.Count);
+    }
+}
+catch (IOException e)
+{
+    Console.WriteLine(e.Message);
+}
