f8b83431848eab36bc83adafb704cfd3aaa222f5
Bruno Bonifácio de Oliveira <bbdoliveira@gmail.com>
2022-01-27 21:35:06 -0300

    Exemplo 1 de Extension methods

---
+++ Exercicios Resolvidos/Exercicio 41 Extension methods/Course/Course/Externsions/DateTimeExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+
+
+namespace System
+{
+    internal static class DateTimeExtensions
+    {
+        public static string ElapsedTime(this DateTime thisObj)
+        {
+            TimeSpan duration = DateTime.Now.Subtract(thisObj); //Subtrai o tempo de agora com que foi setado no DateTime.
+
+            if (duration.TotalHours < 24.0)
+            {
+                return duration.TotalHours.ToString("F1", CultureInfo.InvariantCulture) + " hours";
+            }
+            else
+            {
+                return duration.TotalDays.ToString("F1", CultureInfo.InvariantCulture) + " days";
+            }
+        }
+    }
+}
---
+++ Exercicios Resolvidos/Exercicio 41 Extension methods/Course/Course/Program.cs
@@ -0,0 +1,2 @@
+﻿DateTime dt = new DateTime(2022, 01, 27, 14, 30, 45); //Fomrato (yyyy, MM, dd, hh, mm, ss)
+Console.WriteLine(dt.ElapsedTime());
