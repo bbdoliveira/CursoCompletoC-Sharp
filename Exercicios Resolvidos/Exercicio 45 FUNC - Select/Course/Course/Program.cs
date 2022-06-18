497c13ab49ba2dbe57b2b4389af7f8a8d227dc48
Bruno Bonifácio de Oliveira <bbdoliveira@gmail.com>
2022-06-17 22:54:27 -0300

    Exercicio de Func Resolvido

---
+++ Exercicios Resolvidos/Exercicio 45 FUNC - Select/Course/Course/Program.cs
@@ -0,0 +1,16 @@
+﻿using Course.Entities;
+
+List<Product> list = new List<Product>();
+
+list.Add(new Product("Tv", 900.00));
+list.Add(new Product("Mouse", 50.00));
+list.Add(new Product("Tablet", 350.00));
+list.Add(new Product("HD Case", 80.00));
+
+Func<Product, string> func = p => p.Name.ToUpper();
+
+List<string> result = list.Select(func).ToList();
+foreach (string s in result)
+{
+    Console.WriteLine(s);
+}
