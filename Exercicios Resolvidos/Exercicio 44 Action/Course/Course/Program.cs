1124e110eecb29520d065ef9b11bd4d235a25abf
Bruno Bonifácio de Oliveira <bbdoliveira@gmail.com>
2022-06-17 22:38:40 -0300

    Exercicio de Action Concluido

---
+++ Exercicios Resolvidos/Exercicio 44 Action/Course/Course/Program.cs
@@ -0,0 +1,16 @@
+﻿using Course.Entities;
+
+List<Product> list = new List<Product>();
+
+list.Add(new Product("Tv", 900.00));
+list.Add(new Product("Mouse", 50.00));
+list.Add(new Product("Tablet", 350.00));
+list.Add(new Product("HD Case", 80.00));
+
+Action<Product> act = p => { p.Price += p.Price * 0.1; };
+
+list.ForEach(act);
+foreach (Product p in list)
+{
+    Console.WriteLine(p);
+}
