c99dd40fe39c8b3677cf9cf761c4320684e65494
Bruno Bonifácio de Oliveira <bbdoliveira@gmail.com>
2022-06-17 22:21:56 -0300

    Aula Predicate com erro.

---
+++ Exercicios Resolvidos/Exercicio 43 Comparison T/Course/Course/Program.cs
@@ -0,0 +1,21 @@
+﻿using Course.Entites;
+
+    List<Product> list = new List<Product>();
+    //Product ProductTest = new();
+
+    list.Add(new Product("Tv", 900.00));
+    list.Add(new Product("Mouse", 50.00));
+    list.Add(new Product("Tablet", 350.00));
+    list.Add(new Product("HD Case", 80.00));
+
+    list.RemoveAll(ProductTest);
+    foreach (Product p in list)
+    {
+        Console.WriteLine(p);
+    }
+
+
+public static bool ProductTest(Product p)
+{
+    return p.Price >= 100.00;
+}
