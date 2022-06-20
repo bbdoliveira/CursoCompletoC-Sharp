b998f39604a9ac594a8efbbfb9fe1bf9cd92a30a
Bruno Bonifácio de Oliveira <bbdoliveira@gmail.com>
2022-06-20 14:49:53 -0300

    Commit update

--- Projeto Web MVC/SalesWebMvc/SalesWebMvc/Controllers/HomeController.cs
+++ Projeto Web MVC/SalesWebMvc/SalesWebMvc/Controllers/HomeController.cs
@@ -1,5 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using SalesWebMvc.Models;
+using SalesWebMvc.Models.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -18,6 +18,7 @@ namespace SalesWebMvc.Controllers
         public IActionResult About()
         {
             ViewData["Message"] = "Your application description page.";
+            ViewData["jose"] = "Este é José, um teste. José seu safado!";
 
             return View();
         }
